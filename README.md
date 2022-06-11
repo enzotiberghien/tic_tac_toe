54559e0748a9bc4c74693397c58384cdebfaf9c3
enzotiberghien <enzotiberghien28@gmail.com>
2022-06-11 12:59:27 +0200

    finished

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # tic_tac_toe
-Tic Tac Toe game available in two gamemodes: singleplayer (against an AI) and multiplayer (against a user)
+Tic Tac Toe game available in Multiplayer gamemode.
--- game.js
+++ game.js
@@ -3,14 +3,15 @@ const winnerDisplay = document.querySelector("#winner-display");
 const resetBtn = document.querySelector("#restart");
 const p1Name = document.querySelector("#player1-name");
 const p2Name = document.querySelector("#player2-name");
+const playBtn = document.querySelector("#play-btn");
 
 const GameBoard = (() => {
     const cells = [];
     const buildBoard = () => {
         for (let i = 0; i < 9; i++) {
             const div = document.createElement("div");
-            div.className = "cell"
-            boardContainer.appendChild(div)
+            div.className = "cell";
+            boardContainer.appendChild(div);
             cells.push(div);
         }
     }
@@ -24,28 +25,15 @@ const Player = (name, sign) => {
 
 const Game = (p1, p2) => {
     GameBoard.buildBoard();
-    const player1 = Player("Enzo", "X");
-    const player2 = Player("Brayan", "O");
+    const player1 = p1;
+    const player2 = p2;
     let currentPlayer = player1;
     let gameFinished = false;
     let winner = "Nobody";
     winCases = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]]
 
-    GameBoard.cells.forEach(cell => {
-        cell.addEventListener("click", () => {
-            resetWinnerDisplay();
-            if (cell.textContent !== "") return;
-            cell.textContent = currentPlayer.sign;
-            currentPlayer = currentPlayer === player1 ? player2 : player1;
-            checkWin(GameBoard.cells)
-            if (gameFinished) {
-                displayWinner();
-                resetGame()
-            }
-        })
-    })
 
-    const displayWinner = () => winnerDisplay.textContent = `${winner} win the game !!!`;
+    const displayWinner = () => winnerDisplay.textContent = `${winner} won the game !!!`;
     const resetWinnerDisplay = () => winnerDisplay.textContent = "";
 
     const checkWin = (cells) => {
@@ -57,8 +45,8 @@ const Game = (p1, p2) => {
         });
 
         winCases.forEach(e => {
-            if (e.every(o => xCells.includes(o))) [winner, gameFinished] = [player1.name, true]
-            else if (e.every(o => oCells.includes(o))) [winner, gameFinished] = [player2.name, true]
+            if (e.every(o => xCells.includes(o))) [winner, gameFinished] = [player1.name, true];
+            else if (e.every(o => oCells.includes(o))) [winner, gameFinished] = [player2.name, true];
         })
 
         if (cells.every(e => e.textContent !== "")) gameFinished = true;
@@ -69,12 +57,34 @@ const Game = (p1, p2) => {
         [currentPlayer, gameFinished, winner] = [player1, false, "Nobody"];
     }
 
+    const play = () => {
+        GameBoard.cells.forEach(cell => {
+            cell.addEventListener("click", () => {
+                resetWinnerDisplay();
+                if (cell.textContent !== "") return;
+                cell.textContent = currentPlayer.sign;
+                currentPlayer = currentPlayer === player1 ? player2 : player1;
+                checkWin(GameBoard.cells);
+                if (gameFinished) {
+                    displayWinner();
+                    resetGame();
+                }
+            })
+        })
+    }
+
     return {player1, player2, currentPlayer, gameFinished, winner, winCases, 
-            displayWinner, resetWinnerDisplay, checkWin, resetGame}
+            displayWinner, resetWinnerDisplay, checkWin, resetGame, play};
 }
 
+
+
 const main = (() => {
-    const game = Game(Player("Enzo", "X"), Player("Brayan", "O"));
+    const [p1, p2] = [Player(prompt("X player: "), "X"), Player(prompt("O player: "), "O")];
+    p1Name.textContent = p1.name;
+    p2Name.textContent = p2.name;
+    const game = Game(p1, p2);
+    game.play();
 
     resetBtn.addEventListener("click", () => {
         game.resetGame();
